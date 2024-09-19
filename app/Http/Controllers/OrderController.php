9a8863e4ca15cb633c28656f1962cd52f3b52a7c
NVHieu <hieunguyen098791@gmail.com>
2024-09-19 20:32:55 +0700

    a

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -400,7 +400,7 @@ class OrderController extends BaseController
     {
         try {
             DB::beginTransaction();
-            $orders = $request->orders;
+            $orders = $request->all();
             foreach ($orders as $order) {
 
                 $print_provider_id = $order['print_provider_id'];
