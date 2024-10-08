3fda5c5fb553e1f2b5940e576882cf5662ed9899
NVHieu <hieunguyen098791@outlook.com>
2024-10-08 08:18:33 +0700

    123

--- database/migrations/2024_09_07_134619_create_shops_table.php
+++ database/migrations/2024_09_07_134619_create_shops_table.php
@@ -14,17 +14,6 @@ return new class extends Migration
         Schema::create('shops', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();;
-            $table->string('token_printify', 1000)->nullable();;
-            $table->string('shop_printify_id')->nullable();;
-            $table->string('token_merchize', 1000)->nullable();;
-            $table->string('email_otb')->nullable();;
-            $table->string('password_otb', 1000)->nullable();;
-            $table->string('email_private')->nullable();;
-            $table->string('password_private', 1000)->nullable();;
-            $table->string('token_hubfulfill', 1000)->nullable();;
-            $table->string('email_lenful')->nullable();;
-            $table->string('password_lenful', 1000)->nullable();;
-            $table->string('shop_lenful_id')->nullable();;
             $table->unsignedBigInteger('created_by')->nullable();
             $table->unsignedBigInteger('updated_by')->nullable();
             $table->timestamps(); 
