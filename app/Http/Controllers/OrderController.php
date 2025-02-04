905ce60502aab373d9840804dbe7aaec363e4ce0
NVHieu <hieunguyen098791@gmail.com>
2025-02-04 11:31:15 +0700

    123

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -1704,7 +1704,7 @@ class OrderController extends BaseController
                     $size,
                     !empty($item->img_1) ? $this->saveImgeDrive($item->img_1) : '',
                     $item->first_name. ' ' . $item->last_name,
-                    $item->address,
+                    !empty($item->apartment) ? $item->apartment . ' '. $item->address : $item->address,
                     $item->city,
                     $item->zip,
                     $item->state ?? '',
