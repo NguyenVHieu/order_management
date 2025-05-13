eaf2bcad2f0265b2e7f9ea4270fbf68cd95a424a
NVHieu <hieunguyen098791@gmail.com>
2025-05-13 11:30:33 +0700

    up

--- app/Repositories/OrderRepository.php
+++ app/Repositories/OrderRepository.php
@@ -121,7 +121,7 @@ class OrderRepository implements OrderRepositoryInterface
                 ->select('users.id', 'users.name AS user_name', 'shops.id AS shop_id', 'shops.name AS shop_name')
                 ->selectRaw('COALESCE(SUM(orders.cost), 0) AS total_cost')
                 ->selectRaw('COALESCE(COUNT(DISTINCT orders.order_number_group)) AS total_order')
-                ->selectRaw('SUM(orders.quantity) AS item_orders')
+                ->selectRaw('CAST(SUM(orders.quantity) AS SIGNED) AS item_orders')
                 ->groupBy('users.id', 'users.name', 'shops.id', 'shops.name')
                 ->orderBy('users.id')
                 ->union(
@@ -146,7 +146,7 @@ class OrderRepository implements OrderRepositoryInterface
         else if ($type === 'shop') 
         {
             $subQuery = DB::table('orders')
-                ->select('orders.shop_id', DB::raw('SUM(orders.quantity) AS item_orders'))
+                ->select('orders.shop_id', DB::raw('CAST(SUM(orders.quantity) AS SIGNED) AS item_orders'))
                 ->selectRaw('COALESCE(COUNT(DISTINCT orders.order_number_group)) AS total_order_mail')
                 ->whereBetween('orders.recieved_mail_at', [$params['start_date'].' 00:00:00', $params['end_date'].' 23:59:59'])
                 ->groupBy('orders.shop_id');
@@ -193,7 +193,7 @@ class OrderRepository implements OrderRepositoryInterface
                 })
                 ->select('teams.id', 'teams.name AS team_name')
                 ->selectRaw('COALESCE(SUM(orders.cost), 0) AS total_cost')
-                ->selectRaw('COALESCE(SUM(orders.quantity)) AS total_order')
+                ->selectRaw('CAST(SUM(orders.quantity) AS SIGNED) AS item_orders')
                 ->selectRaw('COUNT(orders.id) AS item_orders')
                 ->groupBy('teams.id', 'teams.name')
                 ->unionAll(
